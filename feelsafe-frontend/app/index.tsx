3cf2af8aabadb340ca13ebcc40f63c0afdb5356c
Holden Prine <holdenprine404@gmail.com>
2024-09-17 11:22:43 -0700

    added axios for backend frontend link

--- feelsafe-frontend/app/index.tsx
+++ feelsafe-frontend/app/index.tsx
@@ -1,11 +1,24 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { View } from "react-native";
 import Header from "../components/Header";
 import About from "../components/About";
 import Courses from "@/components/Courses";
 import Store from "@/components/Store";
 import Contact from "@/components/Contact";
+import axios from 'axios';
+
 
 export default function Index() {
+
+  const tempURL = 'http://localhost:3000/';
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get(tempURL).then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
   return (
     <View
       style={{
