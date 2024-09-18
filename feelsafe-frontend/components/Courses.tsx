220fc6e61d2340c42e3ba16290bf8a33def876ac
Holden Prine <holdenprine404@gmail.com>
2024-09-17 17:51:26 -0700

    started component struture for homepage courses component

--- feelsafe-frontend/components/Courses.tsx
+++ feelsafe-frontend/components/Courses.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { View, StyleSheet, Dimensions } from 'react-native'; 
+import { Carousel } from 'nuka-carousel';
+import axios from 'axios';
+
+const { width: screenWidth } = Dimensions.get('window');
 
 const Courses = () => {
+  const [courses, setCourses] = useState([]);
   return (
-    <div>Courses</div>
+    <View>
+      <Carousel autoplay showDots>
+        <div>{}</div>
+        <div>{}</div>
+        <div>{}</div>
+      </Carousel>
+
+    </View>
   )
 }
 
