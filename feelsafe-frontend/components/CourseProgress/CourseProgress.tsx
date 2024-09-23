024969c0ab86e51807ff556ef830c55a8164dadd
Holden Prine <holdenprine404@gmail.com>
2024-09-23 13:21:01 -0700

    structuring for course progress view

---
+++ feelsafe-frontend/components/CourseProgress/CourseProgress.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const CourseProgress = () => {
+  return (
+    <View>
+        <Text>CourseProgress</Text>
+    </View>
+  )
+}
+
+export default CourseProgress
